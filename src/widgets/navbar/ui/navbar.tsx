3de56b118d5c5c81c7debf3a7d3804b23fab0e26
Егор <124521323+f1k13@users.noreply.github.com>
2023-11-02 12:48:52 +0300

    feat: add switch theme

--- src/widgets/navbar/ui/navbar.tsx
+++ src/widgets/navbar/ui/navbar.tsx
@@ -1,15 +1,22 @@
 import { NavbarIconTheme, NavbarLogo } from "@/shared/ui/icons";
 import styles from "../styles/navbar.module.scss";
-
+import { useLayoutEffect, useState } from "react";
 
 const Navbar = () => {
-  
+  const [theme, setTheme] = useState("dark");
+
+  const switchTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+  useLayoutEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+  }, [theme]);
   return (
     <div className={styles.root}>
-      <div >
-        <NavbarLogo />
+      <NavbarLogo />
+      <div onClick={() => switchTheme()}>
+        <NavbarIconTheme theme={theme}/>
       </div>
-      <NavbarIconTheme />
     </div>
   );
 };
